8ec1c76e44c0ab17f1c61ba8754bf79f409755c0
Dragicevic09 <Sasadragicevic1995@gmail.com>
2023-08-27 10:44:54 -0700

    readme

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+    res.send('Hello world!');
+});
 
-app.listen(3000)
+// Handling 404 Page for all routes
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>');
+});
+
+// Using environment variable for port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
